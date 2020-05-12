d27a49421006ab0e4907a6e3b0d20744ef2326ec
ItoMasaki88 <satarboad10@gmail.com>
2020-05-13 06:31:00 +0900

    ユーザー一覧　bootstrap修正

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -3,29 +3,31 @@
 @section('content')
   <div class="container">
     <h2>ユーザー一覧</h2>
-    <div class="table-responsive">
-      <table class="table table-light">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>氏名</th>
-            <th>性別</th>
-            <th>年齢</th>
-            <th>メールアドレス</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach($users as $user)
-          <tr>
-            <td>{{$user->id}}</td>
-            <td>{{$user->name}}</td>
-            <td>{{$user->text_sex}}</td>
-            <td>{{$user->age}}</td>
-            <td>{{$user->email}}</td>
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
+    <div class="container">
+      <div class="table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              <th class="col-xs-1">ID</th>
+              <th class="col-xs-3">氏名</th>
+              <th class="col-xs-1">性別</th>
+              <th class="col-xs-1">年齢</th>
+              <th class="col-xs-3">メールアドレス</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($users as $user)
+            <tr>
+              <td>{{$user->id}}</td>
+              <td>{{$user->name}}</td>
+              <td>{{$user->text_sex}}</td>
+              <td>{{$user->age}}</td>
+              <td>{{$user->email}}</td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 @endsection
